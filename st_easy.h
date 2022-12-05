329db29bee9f0c8637ddabbdaea2d7567f8e50b4
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2022-12-05 13:50:23 +0300

    Add files via upload

---
+++ st_easy.h
@@ -0,0 +1,20 @@
+#ifndef ST,EASY_H_INCLUDED
+#define ST,EASY_H_INCLUDED
+#include <iostream>
+#include <string>
+using namespace std;
+string itc_hello_str(string st);
+long long itc_len(string st);
+void itc_print_copy_str(string st, int a1);
+void itc_first_end_three(string st);
+int itc_count_char_in_str(char ch, string st);
+string itc_even_place(string st);
+double itc_percent_lower_uppercase(string st);
+string itc_reverse_str(string st);
+string itc_slice_str(string st, int a1, int a2);
+bool itc_equal_reverse(string st);
+string itc_cmp_str(string st, string st2, int a1);
+int itc_find_str(string st, string st2);
+string itc_three_str(string st, string st2, string st3);
+int itc_max_char_on_end(string st);
+#endif
---
+++ st_easy1.cpp
@@ -0,0 +1,35 @@
+#include"st,easy.h"
+string itc_hello_str(string st){
+return ("Hello, " + st);
+}
+long long itc_len(string st) {
+  long long a1 = 0;
+  for (int i = 0; st[i] != '\0'; i++) {
+    a1++;
+  }
+  return a1;
+}
+void itc_print_copy_str(string st, int a1) {
+  for (int i = 0; i < a1; i++)
+    cout << st;
+}
+void itc_first_end_three(string st) {
+  long long a1 = itc_len(st);
+  if (a1 > 5) {
+    cout << st[0] << st[1] << st[2] << st[a1 - 3]
+         << st[a1 - 2] << st[a1 - 1];
+  } else {
+    while (a1 > 0) {
+      cout << st[0];
+      a1--;
+    }
+  }
+}
+int itc_count_char_in_str(char ch, string st) {
+  int a1 = 0;
+  for (int i = 0; i <= itc_len(st); i++) {
+    if (ch == st[i])
+      a1++;
+  }
+  return a1;
+}
